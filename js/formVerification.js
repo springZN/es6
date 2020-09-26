d2418ec34d46abfa9a40a7591324240377f48f77
springZN <68208271+springZN@users.noreply.github.com>
2020-09-26 12:30:21 +0800

    表单验证的js代码

---
+++ js/formVerification.js
@@ -0,0 +1,84 @@
+window.addEventListener('load', function() {
+    // 手机号的正则表单式
+    var regp = /^(1[3]|[4]|[7]|[8])\d{9}$/;
+    // qq号的正则表单式
+    var regqq = /^[1-9]\d{4,}$/;
+    // 昵称的正则表单式
+    var reguser = /^[\u4e00-\u9fa5a-zA-Z0-9]+$/;
+    // 短信验证码的正则表单式
+    var regnote = /^[\da-zA-Z]{6}$/;
+    // 密码的正则表单式
+    var regpass = /^[a-zA-Z0-9_-]{6,16}$/;
+    // 有多个元素要添加事件 可以封装在一个函数里面
+    function verification(el, reg) {
+        el.oninput = function() {
+            if (reg.test(this.value)) {
+                this.nextElementSibling.innerHTML = '&#xe62a;输入的格式正确'
+                    // 不能使用 classList 因为使用classList添加不会覆盖前面的类名 只会在后面添加 这样输入不正确格式以后 再输入正确的格式 不正确的类会覆盖正确的 
+                this.nextElementSibling.className = 'iconfont verification correct';
+                // 这里不太适合使用display：block 因为会把元素转换为块级 从而影响布局
+                this.nextElementSibling.style.visibility = 'visible'
+                el.onblur = function() {
+                    this.nextElementSibling.style.visibility = 'hidden'
+                }
+            } else {
+                switch (el) {
+                    case phone:
+                        this.nextElementSibling.innerHTML = '&#xe71c; 输入的格式应该为13、14、17、18开头的11位数字'
+                        break;
+                    case qq:
+                        this.nextElementSibling.innerHTML = '&#xe71c; qq号最少是五位数字，且第一位数字大于等于1'
+                        break;
+                    case nickname:
+                        this.nextElementSibling.innerHTML = '&#xe71c; 最少一个字符，最多八个字符，不包含特殊符号'
+                        break;
+                    case veriCode:
+                        this.nextElementSibling.innerHTML = '&#xe71c; 短信验证码是6位数字'
+                        break;
+                    case password:
+                        this.nextElementSibling.innerHTML = '&#xe71c; 由6-16位数字 字母（支持大小写） -  _  组成'
+                        break;
+                }
+                this.nextElementSibling.className = 'iconfont verification incorrect'
+                this.nextElementSibling.style.visibility = 'visible'
+            }
+        }
+    }
+    var phone = document.querySelector('.phone')
+    var qq = document.querySelector('.qq')
+    var nickname = document.querySelector('.nickname')
+    var veriCode = document.querySelector('.veriCode')
+    var password = document.querySelector('.password')
+    var contrast = document.querySelector('.contrast')
+    verification(phone, regp) // 手机号
+    verification(qq, regqq) // qq
+    verification(nickname, reguser) // 昵称
+    verification(veriCode, regnote) // 短信验证码
+    verification(password, regpass); // 密码
+    // 确认密码的事件 
+    contrast.addEventListener('input', function() {
+        // 确认密码和登录密码必须一致且登录密码符合规则 提示密码设置成功
+        if (this.value === password.value && 　regpass.test(password.value)) {
+            this.nextElementSibling.innerHTML = '&#xe62a;密码设置成功'
+            this.nextElementSibling.className = 'iconfont verification correct'
+            this.nextElementSibling.style.visibility = 'visible'
+            this.onblur = function() {
+                this.nextElementSibling.style.visibility = 'hidden'
+            }
+        } else {
+            this.nextElementSibling.innerHTML = '&#xe71c;两次的密码不一致'
+            this.nextElementSibling.className = 'iconfont verification incorrect'
+            this.nextElementSibling.style.visibility = 'visible';
+            // 登录密码不符合规则的时候
+            if (!regpass.test(password.value)) {
+                this.nextElementSibling.innerHTML = '&#xe71c;登录密码不符合规则'
+                this.nextElementSibling.className = 'iconfont verification incorrect'
+            }
+            // 登录密码为空的时候
+            if (password.value == '') {
+                this.nextElementSibling.innerHTML = '&#xe71c;登录密码不能设置为空'
+                this.nextElementSibling.className = 'iconfont verification incorrect'
+            }
+        }
+    })
+})
